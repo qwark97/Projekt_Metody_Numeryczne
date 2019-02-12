bab0a5d2172d24edaff158597c62e62a0896e8a7
qwark97 <37438072+qwark97@users.noreply.github.com>
2019-02-12 12:33:02 +0100

    srednie temeperatury miesięczne

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 from polynomial import Polynomial
 from newton_polynomial import Newton
 from data import Data
+from others import avg
+import matplotlib.pyplot as plt
 
 data = Data('tabelka.csv')
+mcs = {1:'Sty', 2:'Lut', 3:'Mar', 4:'Kwi', 5:'Maj', 6:'Cze', 7:'Lip', 8:'Sie', 9:'Wrz', 10:'Paź', 11:'Lis', 12:'Gru'}
 
+"""
+ŚREDNIE TEMPERATURY MIESIĘCZNE
+""""""
+avgs = []
+for mc_idx, mc in enumerate(data.all_months):
+    avgs.append((mcs[mc_idx+1], avg(mc))) 
+plt.scatter([mc for mc, _ in avgs], [temp for _, temp in avgs], color='Red')
+plt.show()
+for mc, temp in avgs:
+    print(mc, temp, sep=': ')
+""" 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 styczen = data.all_months[0]
 x = Newton([x for x in range(2010, 2017)], styczen)
 x.plot([x for x in range(2010, 2017)], "Temperatury w styczniu w latach 2010-2016", shift_X=2010)
@@ -31,4 +62,4 @@ x.plot([x for x in range(2010, 2017)], "Temperatury w czerwcu w latach 2010-2016
 lipiec = data.all_months[6]
 x = Newton([x for x in range(2010, 2017)], lipiec)
 x.plot([x for x in range(2010, 2017)], "Temperatury w lipcu w latach 2010-2016", shift_X=2010)
-
+'''
---
+++ others.py
@@ -0,0 +1,2 @@
+def avg(lst):
+    return round(sum(lst) / len(lst), 2)
