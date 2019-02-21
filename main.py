195a48716fe95f3cca0772a0ef2835a5d0850e35
qwark97 <37438072+qwark97@users.noreply.github.com>
2019-02-21 22:28:07 +0100

    ostateczna forma przebiegu eksperymentu

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+from itertools import starmap
+from math import ceil
+import matplotlib.pyplot as plt
+import numpy as np
+from integral import Integral
 from polynomial import Polynomial
 from newton_polynomial import Newton
 from newtonraphson_method import NRMethod
 from data import Data
 from others import avg, avg_labeled
-import matplotlib.pyplot as plt
-import numpy as np
-from math import ceil
-from integral import Integral
-from itertools import starmap
+
+
 
 
 data = Data('tabelka.csv')
@@ -24,7 +26,7 @@ plt.scatter(np.arange(1.5, 13.5), list(starmap(lambda _, temp: temp, avgs)), col
 plt.title('Średnie temperatury miesięczne w ciągu roku')
 plt.show()
 
-print('\nTemperatury w stopniach Celcjusza')
+print('\nTemperatury w stopniach Celsjusza')
 for mnth, temp in avgs:
     print(mnth, temp, sep=': ')
 print()
@@ -48,8 +50,8 @@ for i in range(7): # pętla po latach
     plt.scatter(np.arange(1.5, 13.5), all_years[i], color='red', label='Średnie miesięczne temperatury w danym roku') 
     plt.scatter([mnths[t] for t in np.arange(0, 13)], np.arange(0, 13), color='white') 
 
-
     plt.title("Temperatury w ciągu roku"+' 201'+str(i))                
+    plt.legend()
     plt.show()                                                 
 
 
@@ -83,9 +85,12 @@ plt.scatter(np.arange(1.5, 13.5), avgs[2:-2], color='red', label='Średnie miesi
 plt.scatter([mnths[t] for t in np.arange(0, 13)], np.arange(0, 13), color='white') # ta linia jedynie ułatwia mi odpowiednie oznaczenie wykresu
 plt.legend()
 plt.show()
+
+print('Wzór uzyskanego wielomianu:') 
+interpolation.designate_polynomial().show_function()
 '''
 """
-SZUKANIE DNI DLA TEMPERATUR UJEMNYCH ORAZ DODATNICH UŻYWAJĄC METOD PRZYBLIŻONYCH - TUTAJ METODY SIECZNYCH
+SZUKANIE DNI DLA TEMPERATUR UJEMNYCH ORAZ DODATNICH UŻYWAJĄC METOD PRZYBLIŻONYCH - TUTAJ METODY STYCZNYCH
 """ 
 '''
 # ponowne wynaczenie wielomianu wybranego na poprzednim etapie
@@ -94,28 +99,28 @@ avgs.insert(0, avgs[-1])
 avgs.insert(0, avgs[-2]) 
 avgs.append(avgs[2])     
 avgs.append(avgs[3]) 
+interpolation = Newton(np.arange(-0.5, 15.5), avgs)
 
 # utworzenie obiektu klasy wielomian z interpolacji Newtona
-interpolation = Newton(np.arange(-0.5, 15.5), avgs)
 polynomial = interpolation.designate_polynomial() 
-polynomial.plot([1, 13], name='Wielomian przedstawiający zmiany temperatur w ciągu roku', color='green') 
 
 # na bazie wykresu wyznaczam przedziały, w których szukane będą pierwiastki wielomianu
+polynomial.plot([1, 13], name='Wielomian przedstawiający zmiany temperatur w ciągu roku', color='green') 
+
 scopes = [(2, 2.5), (12.5, 13)]
 results = []
 
 for scope in scopes:
     a, b = scope
-    method = NRMethod(polynomial, a, b)
-    approx = method.iterate(eps=10**(-5))
+    method = NRMethod(polynomial, a, b)         # instancja klasy z met. stycznych
+    approx = method.iterate(eps=10**(-5))       # iteracja do wyniku z przyjętą dokładnością zwracająca tablicę kolejnych przybliżeń
     results.append(approx)
 
 for point, approxs in zip(scopes, results):
     print('Kolejne przybliżenia pierwiastka w przedziale', point, ':', '\n', approxs)
 
 # szacuje konkretne dni w których temperatura zmienia się 
-# z ujemnej na dodatnią i na odwrót aby ocenić 
-# kiedy temperatura jest ujemna oraz dodatnia
+# z ujemnej na dodatnią i na odwrót 
 first_change = str(ceil((results[0][-1] - int(results[0][-1]) ) * 30))+'.%02d' % int(results[0][-1])
 secon_change = str(ceil((results[1][-1] - int(results[1][-1]) ) * 30))+'.%02d' % int(results[1][-1])
 
@@ -125,64 +130,65 @@ print(
        Po wspomnianej dacie, średnia temperatura już 
        do końca roku była ujemna.\n""".format(first_change, secon_change)
 )
-
-
-
 '''
 """
 SZUKANIE NAJCIEPLEJSZEGO I NAJZIMNIEJSZEGO DNIA W ROKU
 """
 '''
+# ponowne wynaczenie wielomianu wybranego na poprzednim etapie
 avgs = list(map(avg, data.all_months))   
 avgs.insert(0, avgs[-1])
 avgs.insert(0, avgs[-2]) 
 avgs.append(avgs[2])     
 avgs.append(avgs[3]) 
-
 interpolation = Newton(np.arange(-0.5, 15.5), avgs)
 polynomial = interpolation.designate_polynomial()
 polynomial.plot([1, 13])
+
+#wyznaczenie pochodnej tego wielomianu
 derivative = polynomial.designate_derivative()
+
+#na bazie wykresu wyznaczam przedział, w którym szukane będzie ekstremum lokalne
 derivative.plot([1, 13], name='Wykres pochodnej funkcji')
+scope = [7.7, 7.8]
 
 #szukam y=0 dla pochodnej w przedziale wybranym na bazie wykresu
-scope = [7.7, 7.8]
 method = NRMethod(derivative, scope[0], scope[1])
 approx = method.iterate(eps=10**(-5))
 x = approx[-1]
-min_or_max = ('Znalezione x to maximum lokalne funkcji' if derivative.designate_res_for_x(x-0.001) > 0 
-                                                        else 'Znaleziony x to minimum lokalne funkcji')
-print(min_or_max)
+print('Znalezione x = {} to maximum lokalne funkcji'.format(x) if derivative.designate_res_for_x(x-0.01) > 0 
+                                                    else 'Znaleziony x = {} to minimum lokalne funkcji'.format(x))
+
+# szacuje konkretny dzień kryjący się pod znalezionym ekstremum
 hottest = str(ceil((x - int(x)) * 30))+'.%02d' % int(x)
 print('Najciepleszy dzień w roku przypadał na %s' % hottest)
 
 # jako, że otrzymany wielomian nie ma jako tako minimum lokalnego 
 # szukam najzimniejszego dnia na początku oraz na końcu wykresu
-
 coldest = min((1, polynomial.designate_res_for_x(1)), (13, polynomial.designate_res_for_x(13)), key=lambda x: x[1])[0]
 print('Najzimniejszy dzien w roku przypadał na {}'.format('01.01' if coldest is 1 else '31.12'))
 '''
 """
 WYLICZENIE ZA POMOCĄ ŚREDNIEJ CAŁKOWEJ ŚREDNIEJ TEMPERATURY W ROKU
 """
-
+'''
+# ponowne wynaczenie wielomianu wybranego na poprzednim etapie
 avgs = list(map(avg, data.all_months))   
 avgs.insert(0, avgs[-1])
 avgs.insert(0, avgs[-2]) 
 avgs.append(avgs[2])     
 avgs.append(avgs[3]) 
-
 interpolation = Newton(np.arange(-0.5, 15.5), avgs)
 polynomial = interpolation.designate_polynomial()
 
+# szukanie najlepszego parametru n
 for n in range(1, 21):
     integral = Integral(n, 1, 13, polynomial)
     print('Wartość dla n =', n,': ', integral.solution())
 
 # Wartość całki normuje się dla n = 6 dlatego dla takiego n policzę średnią
-
 integral = Integral(6, 1, 13, polynomial)
 average_temp = integral.average()
 
-print('Średnia dobowa temperatura w roku w Polsce wynosiła ok.', round(average_temp, 2), 'stopni Celsjusza')
-
+print('Średnia dobowa temperatura w roku w Krakowie wynosiła ok.', round(average_temp, 2), 'stopni Celsjusza')
+'''
